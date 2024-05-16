228b98e5a3c44d78516969fbe0ea48769dd5012b
marphco <5993166+marphco@users.noreply.github.com>
2024-05-15 21:55:12 -0400

    fixed css for desktop navigation

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,72 +1,67 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
+import React from 'react';
 import 'aos/dist/aos.css';
-import { Container, Row } from 'react-bootstrap';
 import "../App.css";
-import {Pennywise, Scribella, ProcrastinEater, ArtHistory} from "./assets/img" 
+import { Pennywise, Scribella, ProcrastinEater, ArtHistory } from "./assets/img"; 
 
-
-// useEffect(() => {
-//     AOS.init({
-//       duration : 1500 // Can be adjusted
-//     });
-// }, []);
-
-
+// Array containing details for each project
 const projects = [
     {
         name: 'Pennywise',
         image: `${Pennywise}`,
         githubLink: 'https://github.com/marphco/pennywise',
         liveDemo: 'https://pennywise-ace8592b7d74.herokuapp.com/'
-      },
-      {
-          name: 'ArtHistory',
-          image: `${ArtHistory}`,
-          githubLink: 'https://github.com/marphco/art-history',
-          liveDemo: 'https://marphco.github.io/art-history/'
-        },
-        {
-          name: 'Scribella',
-          image: `${Scribella}`,
-          githubLink: 'https://github.com/marphco/scribella',
-          liveDemo: 'https://scribella-804de12279f0.herokuapp.com/'
-        },
-        {
-          name: 'Procrastin-Eater',
-          image: `${ProcrastinEater}`,
-          githubLink: 'https://github.com/marphco/procrastin-eater',
-          liveDemo: 'https://marphco.github.io/procrastin-eater/'
-        },
-  ];
+    },
+    {
+        name: 'ArtHistory',
+        image: `${ArtHistory}`,
+        githubLink: 'https://github.com/marphco/art-history',
+        liveDemo: 'https://marphco.github.io/art-history/'
+    },
+    {
+        name: 'Scribella',
+        image: `${Scribella}`,
+        githubLink: 'https://github.com/marphco/scribella',
+        liveDemo: 'https://scribella-804de12279f0.herokuapp.com/'
+    },
+    {
+        name: 'Procrastin-Eater',
+        image: `${ProcrastinEater}`,
+        githubLink: 'https://github.com/marphco/procrastin-eater',
+        liveDemo: 'https://marphco.github.io/procrastin-eater/'
+    },
+];
 
-  const Portfolio = () => {
-  return (
-    <div id="portfolio">
-      <div id="projects" className="row justify-content-center">
-        <h2>Projects</h2>
-        {projects.map((project, index) => (
-          <div key={index} className="col-md-6 col-lg-4 mb-4 proj">
-            <div className="proj-container">
-              <img
-                src={project.image}
-                alt={project.name}
-                className="img-fluid rounded"
-                style={{ maxWidth: '100%', height: 'auto' }}
-              />
-              <div className="overlay">
-                <h4>{project.name}</h4>
-                <div className="buttons">
-                  <button onClick={() => window.open(project.githubLink, '_blank')}>GitHub</button>
-                  <button onClick={() => window.open(project.liveDemo, '_blank')}>Live Demo</button> {/* Adjust this button's functionality as needed */}
-                </div>
-              </div>
+const Portfolio = () => {
+    return (
+        <div id="portfolio">
+            <div id="projects" className="row justify-content-center">
+                <h2>Projects</h2>
+                {projects.map((project, index) => (
+                    <div key={index} className="col-md-6 col-lg-4 mb-4 proj">
+                        <div className="proj-container">
+                            {/* Display project image */}
+                            <img
+                                src={project.image}
+                                alt={project.name}
+                                className="img-fluid rounded"
+                                style={{ maxWidth: '100%', height: 'auto' }}
+                            />
+                            <div className="overlay">
+                                {/* Project name */}
+                                <h4>{project.name}</h4>
+                                <div className="buttons">
+                                    {/* Button to GitHub repository */}
+                                    <button onClick={() => window.open(project.githubLink, '_blank')}>GitHub</button>
+                                    {/* Button to live demo */}
+                                    <button onClick={() => window.open(project.liveDemo, '_blank')}>Live Demo</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Portfolio;
