a5acab5247baedb96268685cd101166b2a62b7d1
marphco <5993166+marphco@users.noreply.github.com>
2024-05-15 21:49:27 -0400

    adjusted portfolio section and styled it

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -3,32 +3,70 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Container, Row } from 'react-bootstrap';
 import "../App.css";
+import {Pennywise, Scribella, ProcrastinEater, ArtHistory} from "./assets/img" 
 
-export const Portfolio = () => {
-    useEffect(() => {
-        AOS.init({
-          duration : 1500 // Can be adjusted
-        });
-    }, []);
 
-    return (
-        <Row id="portfolio">
-            <Container className="d-flex flex-column" id="projects">
-                <div className="proj d-flex justify-content-center flex-column">
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/pennywise" target="_blank" rel="noopener noreferrer">PENNYWISE</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/noteverse" target="_blank" rel="noopener noreferrer">NOTEVERSE</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/art-history" target="_blank" rel="noopener noreferrer">ART HISTORY</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/readme-bot" target="_blank" rel="noopener noreferrer">README BOT</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/flexi-social-api" target="_blank" rel="noopener noreferrer">FLEXI SOCIAL API</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/infini-cart" target="_blank" rel="noopener noreferrer">INFINI CART</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/employee-sync" target="_blank" rel="noopener noreferrer">EMPLOYEE SYNC</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/scribella" target="_blank" rel="noopener noreferrer">SCRIBELLA</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/svg-forge" target="_blank" rel="noopener noreferrer">SVG FORGE</a></div>
-                    <div className="proj-title" data-aos="fade-up" data-aos-offset="300"><a href="https://github.com/marphco/sky-sway" target="_blank" rel="noopener noreferrer">SKY SWAY</a></div>
+// useEffect(() => {
+//     AOS.init({
+//       duration : 1500 // Can be adjusted
+//     });
+// }, []);
+
+
+const projects = [
+    {
+        name: 'Pennywise',
+        image: `${Pennywise}`,
+        githubLink: 'https://github.com/marphco/pennywise',
+        liveDemo: 'https://pennywise-ace8592b7d74.herokuapp.com/'
+      },
+      {
+          name: 'ArtHistory',
+          image: `${ArtHistory}`,
+          githubLink: 'https://github.com/marphco/art-history',
+          liveDemo: 'https://marphco.github.io/art-history/'
+        },
+        {
+          name: 'Scribella',
+          image: `${Scribella}`,
+          githubLink: 'https://github.com/marphco/scribella',
+          liveDemo: 'https://scribella-804de12279f0.herokuapp.com/'
+        },
+        {
+          name: 'Procrastin-Eater',
+          image: `${ProcrastinEater}`,
+          githubLink: 'https://github.com/marphco/procrastin-eater',
+          liveDemo: 'https://marphco.github.io/procrastin-eater/'
+        },
+  ];
+
+  const Portfolio = () => {
+  return (
+    <div id="portfolio">
+      <div id="projects" className="row justify-content-center">
+        <h2>Projects</h2>
+        {projects.map((project, index) => (
+          <div key={index} className="col-md-6 col-lg-4 mb-4 proj">
+            <div className="proj-container">
+              <img
+                src={project.image}
+                alt={project.name}
+                className="img-fluid rounded"
+                style={{ maxWidth: '100%', height: 'auto' }}
+              />
+              <div className="overlay">
+                <h4>{project.name}</h4>
+                <div className="buttons">
+                  <button onClick={() => window.open(project.githubLink, '_blank')}>GitHub</button>
+                  <button onClick={() => window.open(project.liveDemo, '_blank')}>Live Demo</button> {/* Adjust this button's functionality as needed */}
                 </div>
-            </Container>
-        </Row>
-    );
-}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
