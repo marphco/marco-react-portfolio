9029a6835cd29160b3a925740208b5202ca2ca3b
marphco <5993166+marphco@users.noreply.github.com>
2024-09-01 20:47:32 -0400

    created redirect from marcofr.com to www.marcofr.com

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'redirect-plugin',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.headers.host === 'marcofr.com') {
+            res.writeHead(301, { Location: 'https://www.marcofr.com' + req.url });
+            res.end();
+          } else {
+            next();
+          }
+        });
+      }
+    }
+  ],
   build: {
     outDir: 'dist'
   }
